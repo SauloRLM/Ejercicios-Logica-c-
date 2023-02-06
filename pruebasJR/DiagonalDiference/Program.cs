21f285157dcff28dfc7c0a45c647b899f7964f9b
SauloRLM <86893484+SauloRLM@users.noreply.github.com>
2023-02-05 18:33:44 -0700

    updated pratica

--- pruebasJR/DiagonalDiference/Program.cs
+++ pruebasJR/DiagonalDiference/Program.cs
@@ -16,7 +16,6 @@ using System;
 
 class Result
 {   
-
     public static int diagonalDifference(List<List<int>> arr)
     {        
        int tamMatriz = arr.Count;
@@ -38,33 +37,26 @@ class Result
         int res = Math.Abs(diag1 - diag2);
         return res;
     }
-
 }
-
 class Solution
 {
     public static void Main(string[] args)
-    {
-        
+    {   
         /*
         3
         11 2 4
         4 5 6
         10 8 -12
         */
-
         List<List<int>> arr = new List<List<int>>();
         int n = int.Parse(Console.ReadLine());
         for (int i = 0; i < n; i++)
         {
             List<int> row = new List<int>(Console.ReadLine().Split(' ').Select(a => int.Parse(a)));
             arr.Add(row);
-        }
-                
+        }                
         int result = Result.diagonalDifference(arr);                
-
-        Console.WriteLine(result);
- 
+        Console.WriteLine(result); 
     }
 }
 
--- pruebasJR/countPalabras/Program.cs
+++ pruebasJR/countPalabras/Program.cs
@@ -1,10 +1,26 @@
 ﻿//contador de palabras
 using System.Text.RegularExpressions;
 
-string text = "     un   texto    que   tiene    palabras    ";
-int n = 0;
-text = Regex.Replace(text, @"\s+", " ").Trim(); //para limpiar espacios y una expresion regular
-var words = text.Split(' ');  //para contar palabras
-n = words.Length;
+//string text = "     un   texto    que   tiene    palabras    ";
+//int n = 0;
+//text = Regex.Replace(text, @"\s+", " ").Trim(); //para limpiar espacios y una expresion regular
 
-Console.WriteLine("Numero de palabras: "+n);
+string sentence = "   hello   my  friends  ";
+string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);            
+
+//foreach(var element in words){
+    //Console.WriteLine(element+"");
+//}
+
+
+string newSentence = string.Join(" ", words);
+int lastWordLength = words[words.Length - 1].Length;
+Console.WriteLine("Oración sin espacios en exceso: " + newSentence);
+Console.WriteLine("Última palabra: " + words[words.Length - 1]);
+Console.WriteLine("Longitud de la última palabra: " + lastWordLength);
+
+
+//var words = text.Split(' ');  //para contar palabras
+//n = words.Length;
+
+//Console.WriteLine("Numero de palabras: "+n);
